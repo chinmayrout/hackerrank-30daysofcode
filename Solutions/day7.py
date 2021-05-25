fdc2ebddaf43566d5dd2b8127ed06bf1a68ba467
thechinmayrout <thechinmayrout@gmail.com>
2021-05-24 22:25:07 -0400

    Day 6 and 7

---
+++ Solutions/day7.py
@@ -0,0 +1,47 @@
+# https://www.hackerrank.com/challenges/30-arrays/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    print(*arr[::-1], end = " ")
+
+
+'''
+Inside a function header:
+
+* collects all the positional arguments in a tuple.
+
+** collects all the keyword arguments in a dictionary.
+
+>>> def functionA(*a, **kw):
+       print(a)
+       print(kw)
+
+
+>>> functionA(1, 2, 3, 4, 5, 6, a=2, b=3, c=5)
+(1, 2, 3, 4, 5, 6)
+{'a': 2, 'c': 5, 'b': 3}
+In a function call:
+
+* unpacks a list or tuple into position arguments.
+
+** unpacks a dictionary into keyword arguments.
+
+>>> lis=[1, 2, 3, 4]
+>>> dic={'a': 10, 'b':20}
+>>> functionA(*lis, **dic)  #it is similar to functionA(1, 2, 3, 4, a=10, b=20)
+(1, 2, 3, 4)
+{'a': 10, 'b': 20}
+
+'''
